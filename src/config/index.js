dbcdcaa4e792e95ad9b830fcb6f1aabcf0232189
Jveen-D <d85066965@icloud.com>
2021-06-15 17:09:55 +0800

    test localhost

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
     api_access_key: 'kingofthekill',
-    api: process.env.NODE_ENV === 'production' ? 'http://124.71.57.28:8090/api' : 'http://124.71.57.28:8090/api',
+    api: process.env.NODE_ENV === 'production' ? 'localhost:8090/api' : 'http://124.71.57.28:8090/api',
     timeout: 30
 }
