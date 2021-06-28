7f07b0ba63f4b5bd0e276a605c1713884c4e9c38
Jveen-D <d85066965@icloud.com>
2021-06-28 18:57:16 +0800

    去除normalize.css，因为tailwind基于modern-normalize.css

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import router from "./router";
 import { store } from './store/store.js'
 import App from './App.vue'
-import 'normalize.css'
 import './App.css'
 createApp(App)
     .use(router)
