d18132952772f472478b2dedcb4f7386c9a69964
Jveen-D <d85066965@icloud.com>
2021-06-28 20:27:11 +0800

    样式更新 存在问题：Profile动画方向是反的

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -2,57 +2,63 @@
   <div
       class="md:hidden flex justify-center items-center animate-pulse rounded-md fixed top-2 left-4 w-8 h-8 border-solid border-2 border-gray-400"
       @click="showCategoriesList">
-    <svg class="icon" aria-hidden="true">
+    <svg aria-hidden="true" class="icon">
       <use xlink:href="#icon-gengduo"></use>
     </svg>
   </div>
-  <div
-      :class="[
+  <div :class="[showList ? 'showShadowList md:w-0': '',
+                showList === false ? 'hiddenShadowList' : '',
+                'absolute h-screen bg-black bg-opacity-5 md:bg-transparent']"
+                @click.self="showShadowCategoriesList">
+    <div
+        :class="[
           showList ? 'showList' : 'w-0',
-          showList === false ?'hiddenList' : '',
-          mode === 'light'?'lightMode bg-gradient-to-t from-regal-blue to-regal-pink':'darkMode ',
-          'h-screen overflow-hidden items-center fixed top-0 bg-white ' +
-          'md:w-2/4 md:flex md:h-20 md:w-full md:bg-opacity-70 z-10']">
-    <div class="md:hidden relative mt-4" @click="showCategoriesList">
-      <svg class="icon animate-bounce absolute right-4" aria-hidden="true">
-        <use xlink:href="#icon-cha"></use>
-      </svg>
-    </div>
-    <div class="flex flex-col flex-1 justify-between items-center pr-6 md:flex-row md:h-20">
-      <div class="pl-12 pt-4 md:flex md:pt-0">
-        <div v-for="(item,index) in listCategories"
-             :key="'listCategories'+index"
-             :class="[slug === item.slug?'text-FF9100':'',
+          showList === false ? 'hiddenList' : '',
+          mode === 'light'?'lightMode bg-gradient-to-t from-regal-blue to-regal-pink bg-red-400':'darkMode ',
+          'h-screen overflow-hidden items-center fixed top-0 ' +
+          'md:flex md:h-20 md:w-full md:bg-opacity-70 z-10']">
+      <div class="md:hidden relative mt-4" @click="showCategoriesList">
+        <svg aria-hidden="true" class="icon animate-bounce absolute right-4">
+          <use xlink:href="#icon-cha"></use>
+        </svg>
+      </div>
+      <div class="flex flex-col flex-1 justify-between items-center pr-6 md:flex-row md:h-20">
+        <div class="pl-12 pt-4 md:flex md:pt-0">
+          <div v-for="(item,index) in listCategories"
+               :key="'listCategories'+index"
+               :class="[slug === item.slug?'text-FF9100':'',
              mode === 'light'?'text-black':'',
              'font-mersan whitespace-nowrap mt-2 md:mt-0 font-medium text-sm pr-4 transition-colors duration-500 ease-in-out  hover:text-FF9100']"
-             @click="goCategory(index,item.slug)">
-          {{ item.name }}
+               @click="goCategory(index,item.slug)">
+            {{ item.name }}
+          </div>
         </div>
-      </div>
-      <div class="w-full justify-end md:justify-between md:w-auto flex items-center" @click="changeMode">
-        <div class="relative flex justify-between items-center w-20 h-8 bg-gray-100 rounded-md">
-          <div class="z-20 flex justify-center items-center w-10 h-full rounded-md">
-            <svg :class="[mode === 'light'?'animate-bounce mt-1':'','icon']" aria-hidden="true">
-              <use xlink:href="#icon-taiyang"></use>
-            </svg>
-          </div>
-          <div class="z-20 flex justify-center items-center w-10 h-full">
-            <svg :class="[mode === 'light'?'':'animate-bounce mt-1','icon']" aria-hidden="true">
-              <use xlink:href="#icon-yueliang"></use>
-            </svg>
-          </div>
-          <div
-              :class="[mode === 'light'?'light':'dark','z-10 absolute flex justify-center items-center bg-white w-10 h-8 rounded-md']">
+        <div class="w-full justify-end md:justify-between md:w-auto flex items-center" @click="changeMode">
+          <div class="relative flex justify-between items-center w-20 h-8 bg-gray-100 rounded-md">
+            <div class="z-20 flex justify-center items-center w-10 h-full rounded-md">
+              <svg :class="[mode === 'light'?'animate-bounce mt-1':'','icon']" aria-hidden="true">
+                <use xlink:href="#icon-taiyang"></use>
+              </svg>
+            </div>
+            <div class="z-20 flex justify-center items-center w-10 h-full">
+              <svg :class="[mode === 'light'?'':'animate-bounce mt-1','icon']" aria-hidden="true">
+                <use xlink:href="#icon-yueliang"></use>
+              </svg>
+            </div>
+            <div
+                :class="[mode === 'light'?'light':'dark','z-10 absolute flex justify-center items-center bg-white w-10 h-8 rounded-md']">
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+
 </template>
 
 <script>
 import { ListCategories } from './Header'
-import { reactive, toRefs, computed, watch } from "vue"
+import { computed, reactive, toRefs, watch } from "vue"
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
 import { preventScrollY } from '@/utils/utils'
@@ -98,16 +104,19 @@ export default {
     }
     // 移动端显示分类
     const showCategoriesList = () => state.showList = !state.showList
+    // 移动端显示分类蒙层
+    const showShadowCategoriesList = () => state.showList = !state.showList
     return {
       goCategory,
       changeMode,
       showCategoriesList,
+      showShadowCategoriesList,
       ...toRefs(state)
     }
   }
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 @import "Header.scss";
 </style>
--- src/components/Profile/Profile.vue
+++ src/components/Profile/Profile.vue
@@ -4,116 +4,121 @@
       @click="showProfileWrap">
     <img :src="profile.user.avatar" alt="avatar" class="rounded-full"/>
   </div>
-  <div
-      :class="[
+  <div :class="[showProfile ? 'showShadowProfile md:w-0' : '',
+        showProfile === false ? 'hiddenShadowProfile' : '',
+        'absolute h-screen bg-black bg-opacity-5 md:bg-transparent']"
+       @click.self="showShadowProfileWrap">
+    <div
+        :class="[
       showProfile ? 'showProfile' : 'w-0',
       showProfile === false ?'hiddenProfile' : '',
       'fixed right-0 md:static md:inset-0 md:block overflow-hidden font-mersan flex h-full justify-between flex-col md:mt-28 md:w-auto whitespace-nowrap rounded-l-xl']">
-    <div :class="[mode === 'light'?'bg-lightMode':'bg-darkMode','rounded-l-xl overflow-hidden duration-500 ease-in-out']">
-      <div :class="[mode === 'light'?'divide-gray-200':'divide-black','font-bold divide-y']">
-        <div class="flex justify-between py-2 ml-4">
-          <div>Profile</div>
-          <div class="md:hidden mt-2 mr-4" @click="showProfileWrap">
-            <svg class="icon animate-bounce" aria-hidden="true">
-              <use xlink:href="#icon-cha"></use>
-            </svg>
+      <div :class="[mode === 'light'?'bg-lightMode':'bg-darkMode','rounded-l-xl overflow-hidden duration-500 ease-in-out']">
+        <div :class="[mode === 'light'?'divide-gray-200':'divide-black','font-bold divide-y']">
+          <div class="flex justify-between py-2 ml-4">
+            <div>Profile</div>
+            <div class="md:hidden mt-2 mr-4" @click="showProfileWrap">
+              <svg class="icon animate-bounce" aria-hidden="true">
+                <use xlink:href="#icon-cha"></use>
+              </svg>
+            </div>
+          </div>
+          <div></div>
+        </div>
+        <div class="flex justify-center items-end  w-full h-24">
+          <img :src="profile.user.avatar" alt="avatar" class="w-20 h-20 rounded-full"/>
+        </div>
+        <div :class="[mode === 'light'?'text-black':'','flex justify-center items-center duration-500 ease-in-out pt-4 font-bold text-sm']">
+          {{ profile.user.nickname }}
+        </div>
+        <div class="flex justify-center items-center  pt-2 text-xs">
+          <svg class="icon animate-ping" aria-hidden="true">
+            <use xlink:href="#icon-12"></use>
+          </svg>
+          <p>南京</p>
+        </div>
+        <div class="flex justify-center items-center  pt-2 text-xs">
+          {{ profile.user.email }}
+        </div>
+        <div class="rounded-l-xl flex pt-4 pb-4 justify-center items-center ">
+          <div class="flex justify-between w-3/6">
+            <a href="weixin://" title="和俺聊天">
+              <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-weixin"></use>
+              </svg>
+            </a>
+            <a target="_blank" href="https://github.com/Jveen-D" title="github">
+              <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-github"></use>
+              </svg>
+            </a>
+            <a target="_blank" href="https://wpa.qq.com/msgrd?v=3&uin=85066965&site=qq&menu=yes" title="和俺聊天">
+              <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-QQ"></use>
+              </svg>
+            </a>
           </div>
         </div>
-        <div></div>
       </div>
-      <div class="flex justify-center items-end  w-full h-24">
-        <img :src="profile.user.avatar" alt="avatar" class="w-20 h-20 rounded-full"/>
-      </div>
-      <div :class="[mode === 'light'?'text-black':'','flex justify-center items-center duration-500 ease-in-out pt-4 font-bold text-sm']">
-        {{ profile.user.nickname }}
-      </div>
-      <div class="flex justify-center items-center  pt-2 text-xs">
-        <svg class="icon animate-ping" aria-hidden="true">
-          <use xlink:href="#icon-12"></use>
-        </svg>
-        <p>南京</p>
-      </div>
-      <div class="flex justify-center items-center  pt-2 text-xs">
-        {{ profile.user.email }}
-      </div>
-      <div class="rounded-l-xl flex pt-4 pb-4 justify-center items-center ">
-        <div class="flex justify-between w-3/6">
-          <a href="weixin://" title="和俺聊天">
-            <svg class="icon" aria-hidden="true">
-              <use xlink:href="#icon-weixin"></use>
-            </svg>
-          </a>
-          <a target="_blank" href="https://github.com/Jveen-D" title="github">
-            <svg class="icon" aria-hidden="true">
-              <use xlink:href="#icon-github"></use>
-            </svg>
-          </a>
-          <a target="_blank" href="https://wpa.qq.com/msgrd?v=3&uin=85066965&site=qq&menu=yes" title="和俺聊天">
-            <svg class="icon" aria-hidden="true">
-              <use xlink:href="#icon-QQ"></use>
-            </svg>
-          </a>
-        </div>
-      </div>
-    </div>
-    <div :class="[mode === 'light'?'bg-lightMode':'bg-darkMode','mt-2 rounded-l-xl w-full pb-2 duration-500 ease-in-out']"
-         style="width: 300px;">
-      <div class="text-xs font-bold px-4 pt-2">博客技术细节:</div>
-      <div class="flex pl-2 pt-2">
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#icon-biaoqian"></use>
-        </svg>
-        <div class="text-sm">:使用Vue3 Components API</div>
-      </div>
-      <div class="flex pl-2 pt-2">
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#icon-biaoqian"></use>
-        </svg>
-        <div class="text-sm">:使用Tailwind Css响应式布局</div>
-      </div>
-      <div class="flex pl-2 pt-2">
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#icon-biaoqian"></use>
-        </svg>
-        <div class="text-sm">:代码支持高亮显示</div>
-      </div>
-      <div class="flex pl-2 pt-2">
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#icon-biaoqian"></use>
-        </svg>
-        <div class="text-sm">:使用Docker创建Jenkins持续集成</div>
-      </div>
-      <div class="flex pl-2 pt-2">
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#icon-biaoqian"></use>
-        </svg>
-        <div class="text-sm">:使用Docker创建Nginx服务</div>
-      </div>
-      <div class="flex pl-2 pt-2">
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#icon-biaoqian"></use>
-        </svg>
-        <div class="text-sm">:使用shell脚本实现自动化部署</div>
-      </div>
-    </div>
-    <div
-        :class="[mode === 'light'?'bg-lightMode':'bg-darkMode','flex flex-col justify-between h-full flex-1  rounded-l-xl w-full  mt-2 text-sm duration-500 ease-in-out']">
-      <div>
-        <div class="text-xs font-bold px-4 py-2">Other:</div>
-        <div class="flex items-center pl-2">
+      <div :class="[mode === 'light'?'bg-lightMode':'bg-darkMode','mt-2 rounded-l-xl w-full pb-2 duration-500 ease-in-out']"
+           style="width: 300px;">
+        <div class="text-xs font-bold px-4 pt-2">博客技术细节:</div>
+        <div class="flex pl-2 pt-2">
           <svg class="icon" aria-hidden="true">
-            <use xlink:href="#icon-shijian"></use>
+            <use xlink:href="#icon-biaoqian"></use>
           </svg>
-          <div class="text-sm">博客已运行:</div>
+          <div class="text-sm">:使用Vue3 Components API</div>
         </div>
-        <div class="flex justify-end pr-2">{{ time }}</div>
-        <div class="flex items-center pl-2">
+        <div class="flex pl-2 pt-2">
           <svg class="icon" aria-hidden="true">
-            <use xlink:href="#icon-renshu"></use>
+            <use xlink:href="#icon-biaoqian"></use>
           </svg>
-          <div class="text-sm">累计访问:</div>
+          <div class="text-sm">:使用Tailwind Css响应式布局</div>
         </div>
-        <div class="flex justify-end pr-2">{{ profile.visitCount }}</div>
+        <div class="flex pl-2 pt-2">
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-biaoqian"></use>
+          </svg>
+          <div class="text-sm">:代码支持高亮显示</div>
+        </div>
+        <div class="flex pl-2 pt-2">
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-biaoqian"></use>
+          </svg>
+          <div class="text-sm">:使用Docker创建Jenkins持续集成</div>
+        </div>
+        <div class="flex pl-2 pt-2">
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-biaoqian"></use>
+          </svg>
+          <div class="text-sm">:使用Docker创建Nginx服务</div>
+        </div>
+        <div class="flex pl-2 pt-2">
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-biaoqian"></use>
+          </svg>
+          <div class="text-sm">:使用shell脚本实现自动化部署</div>
+        </div>
+      </div>
+      <div
+          :class="[mode === 'light'?'bg-lightMode':'bg-darkMode','flex flex-col justify-between h-full flex-1  rounded-l-xl w-full  mt-2 text-sm duration-500 ease-in-out']">
+        <div>
+          <div class="text-xs font-bold px-4 py-2">Other:</div>
+          <div class="flex items-center pl-2">
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-shijian"></use>
+            </svg>
+            <div class="text-sm">博客已运行:</div>
+          </div>
+          <div class="flex justify-end pr-2">{{ time }}</div>
+          <div class="flex items-center pl-2">
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-renshu"></use>
+            </svg>
+            <div class="text-sm">累计访问:</div>
+          </div>
+          <div class="flex justify-end pr-2">{{ profile.visitCount }}</div>
+        </div>
       </div>
     </div>
   </div>
@@ -147,8 +152,10 @@ export default {
     }, 1000)
 
     const showProfileWrap = () => state.showProfile = !state.showProfile
+    const showShadowProfileWrap = () => state.showProfile = !state.showProfile
     return {
       showProfileWrap,
+      showShadowProfileWrap,
       ...toRefs(state)
     }
   }
