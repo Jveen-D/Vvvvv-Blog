927955153516ff5bbfd97c42f3ad1b5eeb7b8f0d
Jveen-D <d85066965@icloud.com>
2021-09-18 17:30:36 +0800

    ts打包测试

---
+++ dist/assets/index.85b00c62.js
@@ -0,0 +1,1 @@
+import{d as s,l as a,o as e,e as t,j as o}from"./vendor.ce5e3fda.js";const r={class:"w-full mt-8"};var l=s({setup(s){const l=a("404");return(s,a)=>(e(),t("div",r,o(l.value),1))}});export default l;
