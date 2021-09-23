c5cf9b668c63ede31a4c23cb2c28fe2bab826380
Jveen-D <d85066965@icloud.com>
2021-09-23 09:55:04 +0800

    修改typescript配置

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -1,34 +1,58 @@
-module.exports = {
+// @ts-check
+const { defineConfig } = require('eslint-define-config');
+module.exports = defineConfig({
   root: true,
-  parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: 'module',
-  },
   env: {
     browser: true,
     node: true,
     es6: true,
   },
-  // extends: ["plugin:vue/essential"],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    jsxPragma: 'React',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   extends: [
     'plugin:vue/vue3-recommended',
-    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'prettier',
     'plugin:prettier/recommended',
+    'plugin:jest/recommended',
   ],
-
-  // add your custom rules here
-  //it is base on https://github.com/vuejs/eslint-config-vue
   rules: {
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
     'vue/custom-event-name-casing': 'off',
     'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
     'no-unused-vars': [
       'error',
-      // we are only using this rule to check for unused arguments since TS
-      // catches unused variables but not args.
-      { varsIgnorePattern: '.*', args: 'none' },
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
     ],
     'space-before-function-paren': 'off',
+
     'vue/attributes-order': 'off',
     'vue/one-component-per-file': 'off',
     'vue/html-closing-bracket-newline': 'off',
@@ -51,4 +75,4 @@ module.exports = {
       },
     ],
   },
-};
+});
--- src/pages/categoryLists/categoryLists.ts
+++ src/pages/categoryLists/categoryLists.ts
@@ -1,13 +1,13 @@
 import { listsPostsByCategorySlug, listsPostsByTagSlug } from "/@/api/index";
 // 获取文章列表
-export function ListsPostsByCategorySlug(slug) {
+export const ListsPostsByCategorySlug = (slug):any => {
   return listsPostsByCategorySlug(slug).then((res) => {
     return res.data.data;
   });
 }
 
 // 根据slug获取文章列表
-export function ListsPostsByTagSlug(slug) {
+export const ListsPostsByTagSlug = (slug):any => {
   return listsPostsByTagSlug(slug).then((res) => {
     //        console.log(res.data.data)
     return res.data.data;
