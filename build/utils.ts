75cef764b7055ae6daf543700c09f03a209a7f49
Jveen-D <d85066965@icloud.com>
2021-10-16 15:08:02 +0800

    优化vite配置

---
+++ build/utils.ts
@@ -0,0 +1,27 @@
+// 读取所有环境变量配置文件到process.env
+export function wrapperEnv(envConf: Recordable): ViteEnv {
+    const ret: any = {};
+
+    for (const envName of Object.keys(envConf)) {
+        let realName = envConf[envName].replace(/\\n/g, '\n');
+        realName = realName === 'true' ? true : realName === 'false' ? false : realName;
+
+        if (envName === 'VITE_PORT') {
+            realName = Number(realName);
+        }
+        if (envName === 'VITE_PROXY' && realName) {
+            try {
+                realName = JSON.parse(realName.replace(/'/g, '"'));
+            } catch (error) {
+                realName = '';
+            }
+        }
+        ret[envName] = realName;
+        if (typeof realName === 'string') {
+            process.env[envName] = realName;
+        } else if (typeof realName === 'object') {
+            process.env[envName] = JSON.stringify(realName);
+        }
+    }
+    return ret;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,60 @@
+import type { UserConfig, ConfigEnv } from 'vite';
+import { loadEnv } from 'vite';
 const { resolve } = require('path');
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import viteCompression from 'vite-plugin-compression';
+import { createProxy } from './build/vite/proxy';
+import { wrapperEnv } from './build/utils';
+import { OUTPUT_DIR } from './build/constant';
+
 function pathResolve(dir: string) {
   return resolve(process.cwd(), '.', dir);
 }
-export default defineConfig({
-  plugins: [vue(), viteCompression()],
-  build: {
-    brotliSize: false,
-  },
-  resolve: {
-    alias: [
-      // /@/xxxx => src/xxxx
-      {
-        find: /\/@\//,
-        replacement: pathResolve('src') + '/',
+
+export default ({ mode }: ConfigEnv): UserConfig => {
+  const root = process.cwd();
+
+  const env = loadEnv(mode, root);
+
+  // 由loadEnv读取的布尔类型是一个字符串。此函数可以转换为布尔类型
+  const viteEnv = wrapperEnv(env);
+
+  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, VITE_DROP_CONSOLE } = viteEnv;
+
+  return{
+    base: VITE_PUBLIC_PATH,
+    root,
+    plugins: [vue(), viteCompression()],
+    build: {
+      target: 'es2015',
+      outDir: OUTPUT_DIR,
+      terserOptions: {
+        compress: {
+          keep_infinity: true,
+          // 去除console
+          drop_console: VITE_DROP_CONSOLE,
+        },
       },
-      // /#/xxxx => types/xxxx
-      {
-        find: /\/#\//,
-        replacement: pathResolve('types') + '/',
-      },
-    ],
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://124.71.57.28:8090/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+      brotliSize: false,
+      chunkSizeWarningLimit: 2000,
     },
-  },
-});
+    resolve: {
+      alias: [
+        // /@/xxxx => src/xxxx
+        {
+          find: /\/@\//,
+          replacement: pathResolve('src') + '/',
+        },
+        // /#/xxxx => types/xxxx
+        {
+          find: /\/#\//,
+          replacement: pathResolve('types') + '/',
+        },
+      ],
+    },
+    server: {
+      port: VITE_PORT,
+      proxy: createProxy(VITE_PROXY)
+    },
+  }
+}
