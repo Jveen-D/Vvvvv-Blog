a74b49c7dcf30ab1a4bda1327c539fc7e0965799
Jveen-D <d85066965@icloud.com>
2021-11-03 22:37:08 +0800

    chore:优化样式

--- src/pages/utilsGather/utilsGather.vue
+++ src/pages/utilsGather/utilsGather.vue
@@ -3,7 +3,7 @@
     <div
       :class="[
         mode === 'light' ? 'lightMode text-lightMode' : 'bg-darkMode  text-darkMode',
-        'hidden min:block  min:w-64 fixed top-0 left-0 mt-28 rounded-xl hidden bg-white h-util pt-4 pl-4 font-bold whitespace-nowrap font-medium text-sm',
+        'hidden min:block  min:w-64 fixed top-0 left-0 mt-28 rounded-xl  bg-white h-util pt-4 pl-4 whitespace-nowrap font-medium text-sm',
       ]"
     >
       <div v-for="(item, index) in h4Arr" :key="index" class="hover:text-FF9100">
@@ -22,8 +22,17 @@
         ]"
       >
         <span
-          class="absolute top-4 right-4 text-black font-mersan font-medium text-sm transition-colors"
-        >累计看过：{{ postDetail.visits }}</span>
+          class="
+            absolute
+            top-4
+            right-4
+            text-black
+            font-mersan font-medium
+            text-sm
+            transition-colors
+          "
+          >累计看过：{{ postDetail.visits }}</span
+        >
         <div
           ref="markdownBody"
           class="flex-1 w-screen min-w-800 markdown-body p-4 lg:p-0 break-all"
@@ -33,47 +42,47 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { contentApi } from '/@/api/content';
-import { computed, reactive, toRefs, ComputedRef } from 'vue';
-import { useStore } from 'vuex';
-import '/src/assets/css/markdown-body.scss';
+  import { contentApi } from '/@/api/content';
+  import { computed, reactive, toRefs, ComputedRef } from 'vue';
+  import { useStore } from 'vuex';
+  import '/src/assets/css/markdown-body.scss';
 
-interface State {
-  postDetail: {
-    title:string,
-    visits:string,
-    formatContent:string
-  };
-  markdownBody: any;
-  mode:ComputedRef<string>,
-  h4Arr:any
-}
+  interface State {
+    postDetail: {
+      title: string;
+      visits: string;
+      formatContent: string;
+    };
+    markdownBody: any;
+    mode: ComputedRef<string>;
+    h4Arr: any;
+  }
 
-const store = useStore();
-const state = reactive<State>({
-  postDetail: {
-    title:'',
-    visits:'',
-    formatContent:''
-  },
-  markdownBody: null,
-  mode: computed(() => store.state.mode as string),
-  h4Arr: [],
-});
-const { postDetail, markdownBody, mode, h4Arr } = { ...toRefs(state) };
-contentApi('getPostsById', {
-  postId: '191',
-  formatDisabled: false,
-  sourceDisabled: true,
-}).then((res) => {
-  state.postDetail = res.data;
-  document.title = `Vvvvv-Blog! - ${state.postDetail.title}`;
-  state.markdownBody.innerHTML += state.postDetail.formatContent;
-  const pre = Array.from(document.getElementsByTagName('pre'));
-  code = Array.from(document.querySelectorAll('pre code'));
-  pre.forEach((item, index) => {
-    const language = item.children[0].classList[0].split('-')[1].toUpperCase();
-    const html = `<figcaption class="line-numbers-head">
+  const store = useStore();
+  const state = reactive<State>({
+    postDetail: {
+      title: '',
+      visits: '',
+      formatContent: '',
+    },
+    markdownBody: null,
+    mode: computed(() => store.state.mode as string),
+    h4Arr: [],
+  });
+  const { postDetail, markdownBody, mode, h4Arr } = { ...toRefs(state) };
+  contentApi('getPostsById', {
+    postId: '191',
+    formatDisabled: false,
+    sourceDisabled: true,
+  }).then((res) => {
+    state.postDetail = res.data;
+    document.title = `Vvvvv-Blog! - ${state.postDetail.title}`;
+    state.markdownBody.innerHTML += state.postDetail.formatContent;
+    const pre = Array.from(document.getElementsByTagName('pre'));
+    code = Array.from(document.querySelectorAll('pre code'));
+    pre.forEach((item, index) => {
+      const language = item.children[0].classList[0].split('-')[1].toUpperCase();
+      const html = `<figcaption class="line-numbers-head">
               <div class="custom-carbon">
                 <div class="custom-carbon-dot custom-carbon-dot--red"></div>
                 <div class="custom-carbon-dot custom-carbon-dot--yellow"></div>
@@ -86,37 +95,37 @@ contentApi('getPostsById', {
                 </svg>
               </a>
             </figcaption>`;
-    item.insertAdjacentHTML('beforebegin', html);
-    item.classList.add('line-numbers');
+      item.insertAdjacentHTML('beforebegin', html);
+      item.classList.add('line-numbers');
+    });
+    // @ts-ignore
+    hljs.highlightAll();
+    // 取出h4标签集合
+    state.h4Arr = Array.from(document.getElementsByTagName('h4'));
+    state.h4Arr.forEach((item) => {
+      item.innerHTML = `<a name='${item.innerText}'>${item.innerText}</a>`;
+    });
   });
-  // @ts-ignore
-  hljs.highlightAll();
-  // 取出h4标签集合
-  state.h4Arr = Array.from(document.getElementsByTagName('h4'));
-  state.h4Arr.forEach((item) => {
-    item.innerHTML = `<a name='${item.innerText}'>${item.innerText}</a>`;
-  });
-});
 
-let code;
-// @ts-ignore
-window.copy = (index) => {
-  const ele = document.createElement('div');
-  ele.innerHTML = code[index].innerHTML;
-  let copyStr = '';
-  Array.from(ele.innerText).forEach((item) => {
-    copyStr += item;
-  });
-  const textarea = document.createElement('textarea');
-  document.body.appendChild(textarea);
-  textarea.value = copyStr;
-  textarea.select();
-  document.execCommand('Copy'); // 执行浏览器复制命令
-  document.body.removeChild(textarea);
-  alert('已复制');
-};
+  let code;
+  // @ts-ignore
+  window.copy = (index) => {
+    const ele = document.createElement('div');
+    ele.innerHTML = code[index].innerHTML;
+    let copyStr = '';
+    Array.from(ele.innerText).forEach((item) => {
+      copyStr += item;
+    });
+    const textarea = document.createElement('textarea');
+    document.body.appendChild(textarea);
+    textarea.value = copyStr;
+    textarea.select();
+    document.execCommand('Copy'); // 执行浏览器复制命令
+    document.body.removeChild(textarea);
+    alert('已复制');
+  };
 </script>
 
 <style lang="scss" scoped>
-@import "./utilsGather.scss";
+  @import './utilsGather.scss';
 </style>
