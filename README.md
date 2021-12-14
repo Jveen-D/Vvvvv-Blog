254924f9abae322070b077fcf30b90fc04929a80
Jveen-D <d85066965@icloud.com>
2021-12-14 17:05:08 +0800

    chore:去除测试项目资源

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+<!--
+ * @Date: 2021-06-28 10:55:47
+ * @LastEditors: dwj18066042960
+ * @FilePath: \Vvvvv-Blog\README.md
+-->
 # Vvvvv-Blog
 
 ```shell
@@ -10,4 +15,27 @@ npm run dev
 #
 npm run build
 ```
+##### git commit type
+```tex
+feat :新功能 
 
+fix :修复bug  
+
+docs : 文档改变
+
+style : 代码格式改变
+
+refactor :某个已有功能重构
+
+perf :性能优化
+
+test :增加测试/测试用例
+
+revert: 撤销上一次的 commit 
+
+chore: 改变构建流程、或者增加依赖库、工具等
+
+Init：新建库
+
+build :改变了build工具 如 grunt换成了 npm
+```
--- src/pages/notFound/index.vue
+++ src/pages/notFound/index.vue
@@ -1,114 +1,13 @@
 <!--
  * @Date: 2021-07-19 18:56:16
  * @LastEditors: dwj18066042960
- * @LastEditTime: 2021-12-11 16:45:53
+ * @LastEditTime: 2021-12-14 17:03:47
  * @FilePath: \Vvvvv-Blog\src\pages\notFound\index.vue
 -->
 <template>
-  <div class="w-full h-full p-8 bg-gray-600">
-    <div class="w-full h-full ytrlt" ref="myCharts"> </div>
-  </div>
+  <div class="w-full h-full p-8"> 404 </div>
 </template>
 
-<script lang="ts" setup>
-  import { ref, reactive, nextTick } from 'vue';
-  import * as echarts from 'echarts';
-  // import mapbg from '/@/assets/images/home/mapBg.png';
-  import geoJson from '/@/assets/map_s/json/all-worldcopy.json';
-  // 地图中文名称映射
-  // import nameMap from '/@/assets/map_s/json/nameMap.json';
-
-  // 注册地图
-  const worldMapJson = JSON.parse(JSON.stringify(geoJson));
-  echarts.registerMap('world', worldMapJson);
-  interface State {
-    mapChart: any;
-    mapData: any;
-    tootipTimer: any;
-    count: number;
-    visualMapMax: number;
-  }
-  const state = reactive<State>({
-    mapChart: null,
-    mapData: [], // 地图数据
-    tootipTimer: null,
-    count: 0,
-    visualMapMax: 0,
-  });
-  // 地图容器ref
-  const myCharts = ref<any>();
-  const option = {
-    //--------------   提示框 -----------------
-    tooltip: {
-      show: true,
-    },
-    series: [
-      {
-        type: 'map',
-        // geoIndex: 0,
-        map: 'world',
-        // data根据value值切换不同颜色
-        data: [],
-        // 自定义地区名称
-        // nameMap: nameMap,
-        // center: [115.97, 29.71],
-        zoom: 0.8,
-        roam:true,
-        //--------------   标签 -----------------
-        label: {
-          show: true, // 国际地图国家名称太密集了  最好别展示
-          color: '#fff',
-        },
-        //--------------   提示框 -----------------
-        tooltip: {
-          show: true,
-          trigger: 'item',
-          backgroundColor: 'rgb(54, 122, 224)',
-          borderColor: 'rgb(54, 122, 224)',
-          enterable: false,
-          alwaysShowContent: true,
-          formatter: (params) => {
-            if (params.value) return `${params.name}：${params.value}`;
-            else return '';
-          },
-          textStyle: {
-            color: '#69c5ff',
-          },
-        },
-        //--------------  图形样式 底图 -----------------
-        itemStyle: {
-          show: false,
-          focus: 'self',
-          // 区域的颜色
-          areaColor: 'rgba(81,159,232,0.2)',
-          borderColor: '#01B3FD',
-          borderWidth: 2,
-          opacity: 1,
-          zlevel: 5,
-          // 底部的颜色
-          shadowColor: 'rgba(81,159,232,0.2)',
-          // 设置shadow会导致偏移量失效
-          // shadowBlur: 100,
-          // 底部的偏移量
-          shadowOffsetX: '4',
-          shadowOffsetY: '4',
-        },
-        //--------------  高亮状态下的多边形和标签样式 -----------------
-        emphasis: {
-          label: {
-            color: '#69c5ff',
-          },
-          itemStyle: {
-            areaColor: 'rgb(54, 122, 224)',
-          },
-        },
-      },
-    ],
-  };
-  nextTick(() => {
-    state.mapChart = echarts.init(myCharts.value);
-    state.mapChart.setOption(option);
-  });
-</script>
+<script lang="ts" setup></script>
 
 <style lang="scss" scoped></style>
