f7885265bd509a93110575e8cae1c2c1c6b41a12
duwenjie <d85066965@icloud.com>
2022-02-28 21:18:54 +0800

    feat:完善type-challenges

--- src/pages/type-challenges/TypeChallengesBaseCom.vue
+++ src/pages/type-challenges/TypeChallengesBaseCom.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full h-full pb-4 overflow-y-auto text-gray-700 dark:text-gray-200">
-    <div class="px-2 pb-2 border-b border-[#3C3C43] dark:border-[#585458] border-opacity-[0.12]">
+    <div class="px-2 pb-2 border-[#3C3C43] dark:border-[#585458] border-opacity-[0.12]">
       <div class="pb-2">
         <b>题目：</b>
         <!-- 题目等级：easy、medium、hard和extreme -->
@@ -31,10 +31,72 @@
   </div>
 </template>
 <script setup lang="ts">
+  import { contentApi } from '/@/api/content';
+  import { reactive, ref, watch } from 'vue';
+  import { copy, language } from '/@/utils/markdown/code';
+  import { highlight } from '/@/hooks/core/highlight';
   // props
   const props = defineProps<{
     id: number;
   }>();
-  console.log(props.id);
+  const markdownBody = ref<ElRef>(null);
+  interface State {
+    postDetail: any;
+  }
+  const state = reactive<State>({
+    postDetail: {},
+  });
+  watch(
+    () => props.id,
+    (id) => {
+      if (id === 0) return;
+      contentApi('getPostsById', {
+        postId: id,
+        formatDisabled: false,
+        sourceDisabled: true,
+      }).then((res) => {
+        state.postDetail = res.data;
+        document.title = `Vvvvv-Blog! - ` + state.postDetail.title;
+        markdownBody.value.innerHTML += state.postDetail.formatContent;
+        language(markdownBody.value);
+        highlight();
+        // 规避控制台报错
+        try {
+          // @ts-ignore
+          hljs.highlightAll();
+        } catch {}
+      });
+    },
+    { immediate: true }
+  );
+
+  // @ts-ignore
+  window.copy = copy;
 </script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  a {
+    display: inline-block;
+    cursor: pointer;
+    padding-bottom: 2px;
+    text-decoration: none;
+    position: relative;
+    font-weight: 400;
+    color: #a862ea;
+    &:hover:after {
+      transform: scaleX(1);
+      transform-origin: bottom left;
+    }
+    &:after {
+      content: '';
+      position: absolute;
+      width: 98%;
+      height: 2px;
+      bottom: 0;
+      left: 0;
+      transform: scaleX(0);
+      background-color: #a862ea;
+      transform-origin: bottom right;
+      transition: transform 0.3s ease-in-out;
+    }
+  }
+</style>
