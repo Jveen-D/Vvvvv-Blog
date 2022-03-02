ee9e03a02f5daef34fc0b3d84496f46755048460
duwenjie <d85066965@icloud.com>
2022-03-02 15:12:56 +0800

    feat:type-challenges增加交互

--- src/pages/type-challenges/TypeChallengesBaseCom.vue
+++ src/pages/type-challenges/TypeChallengesBaseCom.vue
@@ -23,29 +23,64 @@
       <p class="mt-2 font-bold">例如：</p>
       <slot name="example"></slot>
     </div>
-    <b class="px-2">
-      <el-button>查看答案</el-button>
-      <slot name="answer"></slot>
-    </b>
-    <div ref="markdownBody" class="px-2 markdown-body"></div>
+    <div ref="question" class="px-2 markdown-body"></div>
+    <el-button class="mt-2 ml-2" @click="viewAnswer">查看答案</el-button>
+    <div
+      ref="answer"
+      style="height: var(--height)"
+      :class="['transition-all px-2 markdown-body overflow-hidden']"
+    ></div>
+    <div class="mt-2 mb-2 ml-2 font-bold">Your code</div>
+    <div ref="code" class="px-2 markdown-body">
+      <figcaption class="line-numbers-head">
+        <div class="custom-carbon">
+          <div class="custom-carbon-dot custom-carbon-dot--red"></div>
+          <div class="custom-carbon-dot custom-carbon-dot--yellow"></div>
+          <div class="custom-carbon-dot custom-carbon-dot--green"></div>
+        </div>
+        <div class="language">TYPESCRIPT</div>
+        <a></a>
+      </figcaption>
+      <el-input
+        :autosize="{ minRows: 5 }"
+        type="textarea"
+        class="w-full line-numbers"
+        v-model="state.code"
+      />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
+  import { useElementSize, useCssVar, watchOnce } from '@vueuse/core';
+  import '/src/assets/css/markdown-body.scss';
   import { contentApi } from '/@/api/content';
-  import { reactive, ref, watch } from 'vue';
+  import { reactive, ref, toRefs, watch } from 'vue';
   import { copy, language } from '/@/utils/markdown/code';
   import { highlight } from '/@/hooks/core/highlight';
   // props
   const props = defineProps<{
     id: number;
   }>();
-  const markdownBody = ref<ElRef>(null);
+  const question = ref<ElRef>(null);
+  const answer = ref<ElRef>(null);
+  const code = ref<ElRef>(null);
   interface State {
     postDetail: any;
+    question: string;
+    answer: string;
+    code: string;
+    showAnswer: boolean;
+    answerCssVarheight: number;
   }
   const state = reactive<State>({
     postDetail: {},
+    question: '',
+    answer: '',
+    code: '',
+    showAnswer: false,
+    answerCssVarheight: 0,
   });
+
   watch(
     () => props.id,
     (id) => {
@@ -57,8 +92,11 @@
       }).then((res) => {
         state.postDetail = res.data;
         document.title = `Vvvvv-Blog! - ` + state.postDetail.title;
-        markdownBody.value.innerHTML += state.postDetail.formatContent;
-        language(markdownBody.value);
+        let index = state.postDetail.formatContent.indexOf('<pre>', 1);
+        question.value.innerHTML += state.postDetail.formatContent.slice(0, index);
+        answer.value.innerHTML += state.postDetail.formatContent.slice(index);
+        language(question.value);
+        language(answer.value);
         highlight();
         // 规避控制台报错
         try {
@@ -72,6 +110,30 @@
 
   // @ts-ignore
   window.copy = copy;
+
+  const rect = reactive(useElementSize(answer));
+  const setAnswerHeightVar = useCssVar('--height', answer);
+  const { height } = { ...toRefs(rect) };
+  // 这里只执行一次，确保获取到dom的高度
+  watchOnce(height, (v) => {
+    state.answerCssVarheight = v;
+    // 必须这样隐藏 使用其他的方式隐藏元素会导致获取的dom高度为0
+    setTimeout(() => {
+      setAnswerHeightVar.value = 0 + 'px';
+    });
+  });
+  const viewAnswer = () => {
+    state.showAnswer = !state.showAnswer;
+  };
+  const { showAnswer } = { ...toRefs(state) };
+
+  watch(showAnswer, (v) => {
+    if (v) {
+      setAnswerHeightVar.value = state.answerCssVarheight + 'px';
+    } else {
+      setAnswerHeightVar.value = 0 + 'px';
+    }
+  });
 </script>
 <style scoped lang="scss">
   a {
