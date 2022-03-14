2c4480d70395e363969ce32a74c469e4d76e614c
duwenjie <d85066965@icloud.com>
2022-03-14 18:43:18 +0800

    feat:修改useFavicon

--- src/pages/vueUse/components/Browser/useFavicon.vue
+++ src/pages/vueUse/components/Browser/useFavicon.vue
@@ -1,35 +1,32 @@
 <!--
  * @Date: 2022-01-17 13:08:28
  * @LastEditors: dwj18066042960
- * @FilePath: /Vvvvv-Blog/src/pages/vueUse/components/useFavicon.vue
+ * @FilePath: \Vvvvv-Blog\src\pages\vueUse\components\Browser\useFavicon.vue
 -->
 <template>
-    <AvueUseBaseCom :id="props.id">
-        <template #des>路径为响应式变量</template>
-        <template #codeEffect>
-            <img class="mb-2 min-w-[200px] max-w-[200px] min-h-[240px] max-h-[240px]" :src="icon || ''" />
-            <el-button @click="change">change</el-button>
-        </template>
-    </AvueUseBaseCom>
+  <AvueUseBaseCom :id="props.id">
+    <template #des>路径为响应式变量</template>
+    <template #codeEffect>
+      <img class="mb-2 min-w-[200px] max-w-[200px] min-h-[240px] max-h-[240px]" :src="icon || ''" />
+      <el-button @click="change">change</el-button>
+    </template>
+  </AvueUseBaseCom>
 </template>
 
 <script lang="ts" setup>
-import AvueUseBaseCom from '../AvueUseBaseCom.vue'
-// props
-const props = defineProps<{
+  import { useFavicon } from '@vueuse/core';
+  import AvueUseBaseCom from '../AvueUseBaseCom.vue';
+  import bg from '/@/assets/img/bg.gif';
+  import bg2 from '/@/assets/img/bg2.gif';
+  // props
+  const props = defineProps<{
     id: number;
-}>();
+  }>();
 
-/**
- * 路径为响应式变量，但是要将静态资源放到public文件夹下，
- * 否则打包以后会找不到资源，因为不像vue2可以使用require引入静态资源
- * */
-import { useFavicon } from '@vueuse/core'
-const icon = useFavicon()
-icon.value = '/img/bg.gif'
-const change = () => {
-    if (icon.value === '/img/bg.gif')
-        icon.value = '/img/bg2.gif'
-    else icon.value = '/img/bg.gif'
-}
+  const icon = useFavicon();
+  icon.value = bg;
+  const change = () => {
+    if (icon.value === bg) icon.value = bg2;
+    else icon.value = bg;
+  };
 </script>
